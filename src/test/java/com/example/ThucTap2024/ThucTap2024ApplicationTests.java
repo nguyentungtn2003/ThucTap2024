59de31684079947c7731392a3f24a6bf8b241728
nguyentungtn2003 <rokfarm3431@gmail.com>
2024-11-04 09:19:59 +0700

    Project

---
+++ src/test/java/com/example/ThucTap2024/ThucTap2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThucTap2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThucTap2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
