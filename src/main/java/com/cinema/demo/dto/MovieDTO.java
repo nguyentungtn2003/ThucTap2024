69089ba1e38931241722e5324e1ff3b186961b6d
Nguyen Quang Huy K17 HL <quanghuytn2022@gmail.com>
2024-11-08 10:08:47 +0700

    sprint1

--- src/main/java/com/cinema/demo/dto/MovieDTO.java
+++ src/main/java/com/cinema/demo/dto/MovieDTO.java
@@ -1,5 +1,19 @@
 package com.cinema.demo.dto;
 
-public class MovieDTO {
+import lombok.Data;
+import java.util.Date;
 
+@Data
+public class MovieDTO {
+    private int movieId;
+    private String description;
+    private String productionCompany;
+    private String director;
+    private String title;
+    private String image;
+    private String trailerUrl;
+    private String version;
+    private String actor;
+    private Date releaseDate;
+    private int runningTime;
 }
