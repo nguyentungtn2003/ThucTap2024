a4ba374e68cc76b1352911a92c07b9b4fe367843
thuhoai03 <143514055+thuhoai03@users.noreply.github.com>
2024-11-29 02:35:36 +0700

    Update CinemaRoomRepository.java

--- src/main/java/com/cinema/demo/repository/CinemaRoomRepository.java
+++ src/main/java/com/cinema/demo/repository/CinemaRoomRepository.java
@@ -3,5 +3,7 @@ package com.cinema.demo.repository;
 import com.cinema.demo.entity.CinemaRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CinemaRoomRepository extends JpaRepository<CinemaRoomEntity, Integer> {
+    CinemaRoomEntity findCinemaRoomEntityById(int idRoom);
 }
